82d5c2851e3580b0a52f8114e0c529d32f2f7afd
dhvenema <DVenema@geonovum.nl>
2017-04-19 14:30:47 +0200

    Include local Bilbiography

---
+++ config.js
@@ -0,0 +1,48 @@
+var respecConfig = {
+  specStatus: "GN-DEF",
+  specType: "ST",
+  editors: [{
+    name: "Geonovum",
+    url: "http://www.geonovum.nl/",
+  }],
+  publishDate: "2013-09-24",
+  shortName: "mdprofiel-iso19115",
+  github: "https://github.com/Geonovum/Metadata-ISO19115/",
+  localBiblio: {
+    "ISO19136": {
+      "href": "https://www.iso.org/standard/32554.html",
+      "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+      "authors": [""],
+      "date": "September 2007",
+      "publisher": "International Organization for Standardization",
+    },
+    "ISO19101": {
+      "href": "https://www.iso.org/standard/26002.html",
+      "title": "ISO 19101:2002 Geographic information -- Reference model",
+      "authors": [""],
+      "date": "Juli 2002",
+      "publisher": "International Organization for Standardization",
+    },
+    "ISO19115": {
+      "href": "https://www.iso.org/standard/26020.html",
+      "title": "ISO 19115:2003  Geographic information -- Metadata",
+      "authors": [""],
+      "date": "Mei 2003",
+      "publisher": "International Organization for Standardization",
+    },
+    "ISO19139": {
+      "href": "http://inspire.ec.europa.eu/documents/Metadata/INSPIRE_MD_IR_and_ISO_v1_2_20100616.pdf",
+      "title": " ISO/TS 19139:2007 Geographic information -- Metadata -- XML schema implementation",
+      "authors": [""],
+      "date": "April 2007",
+      "publisher": "International Organization for Standardization",
+    },
+    "INSPIRE-19115-19119": {
+      "href": "http://inspire.ec.europa.eu/documents/Metadata/INSPIRE_MD_IR_and_ISO_v1_2_20100616.pdf",
+      "title": "INSPIRE Metadata Implementing Rules: Technical Guidelines based on EN ISO 19115 and EN ISO 19119",
+      "authors": ["Drafting Team Metadata and European Commission Joint Research Centre"],
+      "date": "26 oktober 2007",
+      "publisher": "European Commission Joint Research Centre",
+    },
+  },
+};
