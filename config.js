a63b91f79d1a6bcbb87d1e7b2af7ed03c76884f8
Paul <paul.vangenuchten@geocat.net>
2017-10-04 16:15:28 +0200

    literatuur referenties verbeterert

--- config.js
+++ config.js
@@ -1,36 +1,51 @@
 var respecConfig = {
   specStatus: "GN-CV",
+  pubDomein:"hi",
   specType: "ST",
   editors: [{
     name: "Geonovum",
     url: "http://www.geonovum.nl/",
   }],
-  publishDate: "2017-00-01",
+  publishDate: "2017-09-01",
   shortName: "mdprofiel-iso19115",
   github: "https://github.com/Geonovum/Metadata-ISO19115/",
   localBiblio: {
-    "ISO19136": {
+    "ISO19136-2007": {
       "href": "https://www.iso.org/standard/32554.html",
       "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
-      "authors": [""],
       "date": "September 2007",
       "publisher": "International Organization for Standardization",
     },
-    "ISO19101": {
+    "ISO19101-2002": {
       "href": "https://www.iso.org/standard/26002.html",
       "title": "ISO 19101:2002 Geographic information -- Reference model",
-      "authors": [""],
       "date": "Juli 2002",
       "publisher": "International Organization for Standardization",
     },
-    "ISO19115": {
+    "ISO19115-2003": {
       "href": "https://www.iso.org/standard/26020.html",
       "title": "ISO 19115:2003  Geographic information -- Metadata",
-      "authors": [""],
       "date": "Mei 2003",
       "publisher": "International Organization for Standardization",
     },
-    "ISO19139": {
+    "ISO19115-2005": {
+      "href": "https://www.iso.org/standard/26020.html",
+      "title": "ISO 19115:2003  Geographic information -- Metadata",
+      "date": "Mei 2003",
+      "publisher": "International Organization for Standardization",
+    },
+    "ISO19115-2008":{
+      "href": "https://www.iso.org/standard/26020.html",
+      "title": "ISO19115:2005/AC:2008  Geographic information – Metadata, Technical Corrigendum 1",
+      "date": "2008",
+      "publisher": "International Organization for Standardization",
+    },
+    "CSW2APISO": {
+      "title":"OpenGIS Catalogue Services Specification 2.0.2 - ISO Metadata Application Profile",
+      "href":"portal.opengeospatial.org/files/?artifact_id=21460",
+      "publisher": "Open Geospatial Consortium",
+    },
+    "ISO19139-2007": {
       "href": "https://www.iso.org/standard/32557.html",
       "title": " ISO/TS 19139:2007 Geographic information -- Metadata -- XML schema implementation",
       "authors": [""],
@@ -44,5 +59,72 @@ var respecConfig = {
       "date": "26 oktober 2007",
       "publisher": "European Commission Joint Research Centre",
     },
-  },
+    "INSPIRE1312-2014":{
+      "title":"Regulation on interoperability of spatial data services",
+      "href":"http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=uriserv%3AOJ.L_.2014.354.01.0008.01.ENG",
+      "date":"10 december 2014",
+      "publisher": "European commission"
+    },
+    "INSPIRE-2017": {
+      "href": "http://inspire.ec.europa.eu/id/document/tg/metadata-iso19139/2.0.1",
+      "title": "Technical Guidance for the implementation of INSPIRE dataset and service metadata based on ISO/TS 19139:2007",
+      "date": "2017-03-02",
+      "publisher": "European Commission Joint Research Centre",
+    },
+    "INSPIRE-2008": {
+      "href": "http://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:32008R1205",
+      "title": "Implementing Rules for Metadata",
+      "date": "3 December 2008",
+      "publisher": "European Commission Joint Research Centre",
+    },
+    "INSPIRE-2010": {
+      "href": "http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:02010R1089-20131230",
+      "title": "Implementing Rules for interoperability of spatial data sets and services, met amendementen uit 2011, 2013 en 2014.",
+      "authors": [""],
+      "date": "February 6, 2011",
+      "publisher": "European commission"
+    },
+    "ISO1064-2014": {
+      "href": "https://www.iso.org/standard/63207.html",
+      "title": "Hexalobular internal driving feature for bolts and screws",
+      "date": "2014-10",
+      "publisher": "International Organization for Standardization"
+    },
+    "ISO8601-2004": {
+      "href": "https://www.iso.org/standard/40874.html",
+      "title": "Data elements and interchange formats -- Information interchange -- Representation of dates and times",
+      "date": "2004-12",
+      "publisher": "International Organization for Standardization"
+    },
+    "ISO19108-2005": {
+      "href": "https://www.iso.org/standard/39891.html",
+      "title": "Geographic information -- Rules for application schema",
+      "date": "2005-06",
+      "publisher": "International Organization for Standardization"
+    },
+    "ISO19128":{
+      "title":"Geographic information -- Web map server interface",
+      "href":"https://www.iso.org/standard/32546.html",
+      "date":"2005-12",
+      "publisher": "International Organization for Standardization"
+    },
+    "ISO19119":{
+      "title":"Geographic information -- Services",
+      "href":"https://www.iso.org/standard/39890.html",
+      "date":"2005-02",
+      "publisher": "International Organization for Standardization"
+    },
+    "ISOTC46":{
+      "title":"Information and documentation",
+      "href":"https://www.iso.org/committee/48750.html",
+      "date": "1947",
+      "publisher": "International Organization for Standardization"
+    },
+    "ISO639-2":{
+      "title":"Codes for the representation of names of languages -- Part 2: Alpha-3 code",
+      "href":"https://www.iso.org/standard/4767.html",
+      "date":"1998-10",
+      "publisher": "International Organization for Standardization"
+    },
+  }
 };
